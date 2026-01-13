4015d09fc18fd8aefc286d5ed876cbad51f5a65a
abdullahalnoman003 <noman22622@gmail.com>
2026-01-13 23:47:15 +0600

    Added Demo Footer, Navbar

---
+++ GameStack-Client/src/layouts/HomeLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const HomeLayout = () => {
+    return (
+        <div>
+        <nav><Navbar></Navbar></nav>
+        <main><Outlet></Outlet></main>
+        <footer>
+            <Footer></Footer>
+        </footer>
+        </div>
+    );
+};
+
+export default HomeLayout;
---
+++ GameStack-Client/src/routes/router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomeLayout from "../layouts/HomeLayout";
+const router = createBrowserRouter([
+{
+    path:"/",
+    element: <HomeLayout></HomeLayout>,
+    children:[
+        {
+            index: true,
+            element:<></>
+        }
+    ]
+}
+])
+export default router;
